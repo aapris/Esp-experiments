0f75313ef0e59ed519cfecea6eb67d543026906b
Aapo Rista <aapo.github@rista.net>
2016-09-12 22:12:44 +0300

    Simplify main script

--- scripts/mqtt_dht/application.lua
+++ scripts/mqtt_dht/application.lua
@@ -1,4 +1,6 @@
 -- file : application.lua
+dallas = require("ds18b20")
+
 local module = {}  
 m = nil
 temp = 0
@@ -8,23 +10,48 @@ data["protocol"] = '1.0.2'
 data["mac"] = wifi.sta.getmac()
 data["chipid"] = node.chipid()
 
+SYS_PROTOCOL = '1.0.2'
+SYS_MAC = wifi.sta.getmac()
+SYS_CHIPID = node.chipid()
+PIN_DHT22 = 1   -- this is D1 in NodeMCU dev board
+PIN_DS = 2      -- this is D2 in NodeMCU dev board
+PIN_BUTTON = 6  -- this is D6 in NodeMCU dev board
+PIN_MOTION = 7  -- this is D7 in NodeMCU dev board
+
 -- to recover from cjson memory alloc issues?
 -- node.egc.setmode(node.egc.ON_ALLOC_FAILURE)
+
+local function get_system_info(data)
+    -- Hardcoded values
+    data["protocol"] = SYS_PROTOCOL
+    data["mac"] = SYS_MAC
+    data["chipid"] = SYS_CHIPID
+    data["ssid"] = config.WIFI_SSID
+    -- System info
+    data["rssi"] = wifi.sta.getrssi()
+    data["uptime"] = tmr.time()
+    data["node_heap"] = node.heap() -- Current available heap size in bytes.
+    -- Send boot reason for first 100 seconds
+    if data["uptime"] < 100 then
+        local _, bootreason = node.bootreason()
+        data["bootreason"] = bootreason
+    end
+    -- Send this on first pub?
+    -- majorVer, minorVer, devVer, chipid, flashid, flashsize, flashmode, flashspeed = node.info()
+end
 
 -- Sends a simple ping to the broker
 local function send_ping()  
     m:publish(config.ENDPOINT .. "ping","id=" .. config.ID,0,0)
 end
 
-local function read_dht22(pin)
-    local data = {}
+local function read_dht22(pin, data)
+--    local data = {}
     local status,temp,hum,temp_decimial,humi_decimial = dht.read(pin)
     data["status"] = status
     if( status == dht.OK ) then
         data["dht22_temp"] = temp
         data["dht22_humi"] = hum
-        -- print("dht22_temp" .. temp)
-        -- print("dht22_humi" .. humi)
     elseif( status == dht.ERROR_CHECKSUM ) then
         print( "DHT Checksum error" )
     elseif( status == dht.ERROR_TIMEOUT ) then
@@ -33,72 +60,27 @@ local function read_dht22(pin)
     return data
 end
 
-function read_ds18b20(pin)
-    local t = require("ds18b20")
-    t.setup(pin)
-    local addrs = t.addrs()
-    local data = {}
-    if (addrs ~= nil) then
-      print("Total DS18B20 sensors: "..table.getn(addrs))
-    end
-
-    for i=1,table.getn(addrs) do
-        local addr = addrs[i]
-        local s = string.format("DS%02x-%02x%02x%02x%02x%02x%02x%02x",
-            addr:byte(1),addr:byte(2),addr:byte(3),addr:byte(4),
-            addr:byte(5),addr:byte(6),addr:byte(7),addr:byte(8))
-        local temp = t.read(addrs[i],t.C)
-        print("Sensor ("..s.."): "..temp.."'C")
-        data[s] = temp
-    end
-    -- Release modile after use
-    t = nil
-    package.loaded["ds18b20"]=nil
-    return data
-end
-
-
 -- Send DHT22 temp + hum and Dallas temperatures
 local function send_sensordata()
-    local dht_data = read_dht22(1) -- read the only DHT22
-    data["dht22_temp"] = dht_data["dht22_temp"]
-    data["dht22_humi"] = dht_data["dht22_humi"]
-    local ds_data = read_ds18b20(2)  -- read all 18b20 sensors
+    local data = {}
+    get_system_info(data)
+    -- Sensors
+    -- DHT22
+    read_dht22(PIN_DHT22, data) -- read the only DHT22
+    -- Dallas 18b20
+    local ds_data = dallas.read_all(PIN_DS)  -- read all 18b20 sensors
     for key,value in pairs(ds_data) do
         data[key] = value
     end
-    data["rssi"] = wifi.sta.getrssi()
-    data["uptime"] = tmr.time()
-    -- Send boot reason for first 100 seconds
-    if data["uptime"] < 100 then
-        local _, bootreason = node.bootreason()
-        data["bootreason"] = bootreason
-    else
-        data["bootreason"] = nil
-    end
-    -- Send this rarely?
-    data["node_heap"] = node.heap() -- Returns the current available heap size in bytes.
-    -- Send this on first pub?
-    -- node.info()
     local ok, msg = pcall(cjson.encode, data)
     print(msg)
-    -- remove sensor keys from global data variable
-    data["dht22_temp"] = nil
-    data["dht22_humi"] = nil
-    data["rssi"] = nil
-    data["uptime"] = nil
-    for key,value in pairs(ds_data) do
-        data[key] = nil
-    end
     m:publish(config.ENDPOINT .. "sensor",msg,0,0)
 end
 
-
 -- Send button press
 local function send_buttonpress(direction)
-    data["rssi"] = wifi.sta.getrssi()
-    data["uptime"] = tmr.time()
-    data["node_heap"] = node.heap() -- Returns the current available heap size in bytes.
+    local data = {}
+    get_system_info(data)
     if direction == nil then
         direction = "button_press"
     end
@@ -106,13 +88,9 @@ local function send_buttonpress(direction)
     data["event"] = direction
     local ok, msg = pcall(cjson.encode, data)
     print(msg)
-    data["rssi"] = nil
-    data["uptime"] = nil
-    data["event"] = nil
     -- this should be really event endpoint or something
     m:publish(config.ENDPOINT .. "event",msg,0,0)
 end
-
 
 -- Sends my id to the broker for registration
 local function register_myself()  
@@ -123,7 +101,6 @@ end
 
 local function mqtt_start()  
     m = mqtt.Client(config.ID, 120, config.USER, config.PASSWORD)
-    data["ssid"] = config.WIFI_SSID
     -- register message callback beforehand
     m:on("message", function(conn, topic, data) 
       if data ~= nil then
@@ -146,38 +123,52 @@ local function mqtt_start()
 end
 
 --button.lua
-buttonPin = 6 -- this is D6 in NodeMCU dev board
 local debounceDelay = 50
 local debounceAlarmId = 1
-gpio.mode(buttonPin,gpio.INT,gpio.PULLUP)
+gpio.mode(PIN_BUTTON,gpio.INT,gpio.PULLUP)
 
 function buttonPressed()
     -- don't react to any interupts from now on and wait 50ms until the interrupt for the up event is enabled
     -- within that 50ms the switch may bounce to its heart's content
-    gpio.trig(buttonPin, "none")
+    gpio.trig(PIN_BUTTON, "none")
     tmr.alarm(debounceAlarmId, debounceDelay, tmr.ALARM_SINGLE, function()
-        gpio.trig(buttonPin, "up", buttonReleased)
+        gpio.trig(PIN_BUTTON, "up", buttonReleased)
     end)
     -- finally react to the down event
     print("Button pressed")
-    send_buttonpress("close_"..buttonPin)
+    send_buttonpress("close_".. PIN_BUTTON)
 end
 
 function buttonReleased()
     -- don't react to any interupts from now on and wait 50ms until the interrupt for the down event is enabled
     -- within that 50ms the switch may bounce to its heart's content
-    gpio.trig(buttonPin, "none")
+    gpio.trig(PIN_BUTTON, "none")
     tmr.alarm(debounceAlarmId, debounceDelay, tmr.ALARM_SINGLE, function()
-        gpio.trig(buttonPin, "down", buttonPressed)
+        gpio.trig(PIN_BUTTON, "down", buttonPressed)
     end)
     -- finally react to the up event
     print("Button released")
-    send_buttonpress("open_"..buttonPin)
+    send_buttonpress("open_".. PIN_BUTTON)
 end
 
+-- PIR sensor
+gpio.mode(PIN_MOTION,gpio.INT,gpio.PULLDOWN)  -- attach interrupt to inpin
+
+function motion_on()
+    print(tmr.time() .. " Motion Detected!")
+    gpio.trig(PIN_MOTION, "down", motion_off)
+    send_buttonpress("motion_on_".. PIN_MOTION)
+end
+
+function motion_off()
+    print(tmr.time() .. " Motion OFF!")
+    gpio.trig(PIN_MOTION, "up", motion_on)
+    send_buttonpress("motion_off_".. PIN_MOTION)
+end
 
 function module.start()
-  gpio.trig(buttonPin, "down", buttonPressed)
+  gpio.trig(PIN_BUTTON, "down", buttonPressed)
+  gpio.trig(PIN_MOTION, "up", motion_on)
   print("mqtt_start()")
   mqtt_start()
 end
